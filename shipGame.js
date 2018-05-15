f9446b7032c36aefb41ffd25dce03ccbc4cb647a
Pink-Lover <13915520924@163.com>
2018-05-15 22:29:08 +0800

    完成UI，开始编写JS

---
+++ shipGame.js
@@ -0,0 +1,12 @@
+var view = {
+    displayMessage: function(msg){
+        var message = document.getElementById('messageArea');
+        message.innerHTML = msg;
+    },
+    displayHit: function(location){
+
+    },
+    displayMiss: function(location){
+
+    }
+};
