986ef6a6705759a4d1940aff1547fd3b465c6b44
Pink-Lover <13915520924@163.com>
2018-05-15 22:42:03 +0800

    改进html使JS生效，成功使用JS改变元素

--- shipGame.js
+++ shipGame.js
@@ -4,9 +4,14 @@ var view = {
         message.innerHTML = msg;
     },
     displayHit: function(location){
-
+        var cell = document.getElementById(location);
+        cell.setAttribute('class','hit');
     },
     displayMiss: function(location){
-
+        var cell = document.getElementById(location);
+        cell.setAttribute('class','miss');
     }
 };
+view.displayMessage("HI");
+view.displayHit("A3");
+view.displayHit("C4");
