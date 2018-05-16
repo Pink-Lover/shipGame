9f79023746244bcdf0c7b585ee2a4cb6428ed779
Pink-Lover <13915520924@163.com>
2018-05-16 11:13:43 +0800

    增加isSunk函数

--- shipGame.js
+++ shipGame.js
@@ -42,6 +42,11 @@ var model = {
         return false;
     },
     isSunk: function(ship){
-
+        for (var i = 0; i < this.shipLength; i++){
+            if (ship.hits[i] !== "hit"){
+                return false;
+            }
+        }
+        return true;
     }
 }
